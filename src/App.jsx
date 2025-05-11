cc2936b18b6a48ffa4ba7cfb020851aaf4db1590
Maletta <smaletta@hotmail.com>
2025-05-11 15:15:48 -0700

    Initial Upload.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import DataTable from './components/DataTable/DataTable.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline p-2">Weather App</h1>
+      <DataTable />
+    </>
+  )
+}
+
+export default App
